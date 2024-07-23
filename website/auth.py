84fab9ed12dd8f340d68208ea3e1cf21612856a6
Simran Badwal <152518597+simranbadwal@users.noreply.github.com>
2024-07-23 13:29:56 -0400

    Create auth.py

---
+++ website/auth.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html", boolean=True)
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(firstName) < 3:
+            flash('First Name must be greater or equal to 3 characters.', category='error')
+        elif len(lastName) < 3:
+            flash('Last Name must be greater or equal to 3 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be greater than 7 characters.', category='error')
+        else:
+            flash('Account Created with Health Sync! Thank You for Joining.', category='success')
+    return render_template("sign_up.html")
